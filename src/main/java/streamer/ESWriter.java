ace2456a5de3bc2970af5696c085f3ab9e6e8c7b
stud-nilstes <nils.tesdal@hist.no>
2016-10-07 14:10:41 +0200

    Fikset litt konfigurasjon

--- src/main/java/streamer/ESWriter.java
+++ src/main/java/streamer/ESWriter.java
@@ -1,11 +1,18 @@
 package streamer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.elasticsearch.action.index.IndexRequestBuilder;
-import org.elasticsearch.client.Client;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -23,22 +30,35 @@ public class ESWriter {
     private TransportClient client;
     
     public ESWriter(String indexName, String typeName) throws Exception {
+        log.info("ESWriter: inititalizing...");
+        
         this.indexName = indexName;
         this.typeName = typeName;   
+               
+        connect();
+        checkIndex(indexName);
+        
+        log.info("ESWriter: ok");
+    }
+
+    private void connect() throws UnknownHostException, Exception {
+        Properties config = getConfig();
+        String cluster = config.getProperty("cluster.name", "elasticsearch");
+        String host = config.getProperty("host", "localhost");
+        int port = Integer.parseInt(config.getProperty("port", "9300"));
         
         Settings settings = Settings.settingsBuilder()
-            .put("cluster.name", "elasticsearch").build();
+                .put("cluster.name", cluster).build();
         client = TransportClient.builder().settings(settings).build();
-	client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
-            
-        boolean exists = client.admin().indices()
-            .prepareExists(indexName)
-            .execute().actionGet().isExists();
-        if(!exists) {
-            client.admin().indices().prepareCreate(indexName).execute().actionGet();
-            log.log(Level.INFO, "ESWriter: created new index {0}", indexName);
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
+    }
+
+    private void checkIndex(String indexName1) {
+        boolean exists = client.admin().indices().prepareExists(indexName1).execute().actionGet().isExists();
+        if (!exists) {
+            client.admin().indices().prepareCreate(indexName1).execute().actionGet();
+            log.log(Level.INFO, "ESWriter: created new index {0}", indexName1);
         }
-        log.info("ESWriter: ok");
     }
     
     public void addStatus(Date createdAt, String user, String text) {
@@ -56,5 +76,15 @@ public class ESWriter {
         } catch(Exception e) {
             log.log(Level.INFO, "Failed to add message to ElasticSearch: user={0}, message={1}, error={2}", new Object[]{user, text, e.getMessage()});
         }
+    }
+
+    private Properties getConfig() throws Exception {
+        File file = new File("es.properties");
+        if(!file.exists()) {
+            return new Properties();
+        }
+        Properties config = new Properties();
+        config.load(new FileInputStream(file));
+        return config;
     }
 }
