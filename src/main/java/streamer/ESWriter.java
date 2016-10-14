057fd7572cdad7d1aa19fe89ed390049b95d862f
stud-nilstes <nils.tesdal@hist.no>
2016-10-14 11:18:48 +0200

    Bruker http-klient Flummi

--- src/main/java/streamer/ESWriter.java
+++ src/main/java/streamer/ESWriter.java
@@ -1,22 +1,17 @@
 package streamer;
 
+import com.google.gson.Gson;
+import com.ning.http.client.AsyncHttpClient;
+import de.otto.flummi.Flummi;
+import de.otto.flummi.request.GsonHelper;
 import java.io.File;
 import java.io.FileInputStream;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.commons.configuration2.Configuration;
-import org.apache.commons.configuration2.FileBasedConfiguration;
-import org.apache.commons.configuration2.PropertiesConfiguration;
-import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
-import org.apache.commons.configuration2.builder.fluent.Parameters;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.common.xcontent.XContentFactory;
+
 
 /**
  * @author nilstes
@@ -27,7 +22,7 @@ public class ESWriter {
 
     private String indexName;
     private String typeName;
-    private TransportClient client;
+    private Flummi client;
     
     public ESWriter(String indexName, String typeName) throws Exception {
         log.info("ESWriter: inititalizing...");
@@ -43,35 +38,30 @@ public class ESWriter {
 
     private void connect() throws UnknownHostException, Exception {
         Properties config = getConfig();
-        String cluster = config.getProperty("cluster.name", "elasticsearch");
-        String host = config.getProperty("host", "localhost");
-        int port = Integer.parseInt(config.getProperty("port", "9300"));
+        String url = config.getProperty("url", "http://localhost:9200");
         
-        Settings settings = Settings.settingsBuilder()
-                .put("cluster.name", cluster).build();
-        client = TransportClient.builder().settings(settings).build();
-        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        client = new Flummi(asyncHttpClient, url);
     }
 
     private void checkIndex(String indexName1) {
-        boolean exists = client.admin().indices().prepareExists(indexName1).execute().actionGet().isExists();
+        boolean exists = client.admin().indices().prepareExists(indexName1).execute().booleanValue();
         if (!exists) {
-            client.admin().indices().prepareCreate(indexName1).execute().actionGet();
+            client.admin().indices().prepareCreate(indexName1).execute();
             log.log(Level.INFO, "ESWriter: created new index {0}", indexName1);
         }
     }
     
     public void addStatus(Date createdAt, String user, String text) {
         try {
-            client.prepareIndex(indexName, typeName)
-                        .setSource(XContentFactory.jsonBuilder()
-                            .startObject()
-                                .field("user", user)
-                                .field("createdAt", createdAt)
-                                .field("message", text)
-                            .endObject()
-                        )
-                .get();
+            client.prepareIndex()
+                    .setIndexName(indexName)
+                    .setDocumentType(typeName)
+                    .setSource(GsonHelper.object(
+                            "user", user,
+                            "createdAt", new Gson().toJson(createdAt),
+                            "message", text))                     
+                    .execute();
             log.log(Level.INFO, "Added message to ElasticSearch: user={0}, message={1}", new Object[]{user, text});
         } catch(Exception e) {
             log.log(Level.INFO, "Failed to add message to ElasticSearch: user={0}, message={1}, error={2}", new Object[]{user, text, e.getMessage()});
